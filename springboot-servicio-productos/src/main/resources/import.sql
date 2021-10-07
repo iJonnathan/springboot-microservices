aaa9e7162fe926efaae7d7686487e56626fc0f42
ioJonnathan <estebanjps@hotmail.com>
2021-10-07 10:13:41 -0500

    tutorial 20

---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO productos (nombre, precio, created_at) VALUES('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Sony', 1000, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Samsung', 900, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Apple', 400, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Hewlett', 700, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Lenovo', 600, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Nike', 100, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Adidas', 200, NOW());
+INSERT INTO productos (nombre, precio, created_at) VALUES('Reebok', 300, NOW());
